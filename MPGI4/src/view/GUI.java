112f6ab45ec29a71c34689f95b2c82c51480e614
Tobi <Tobi@192.168.1.51>
2012-11-04 15:58:04 +0100

    - removed listeners from JTextfields - added functionality to the ActionListener of the savebutton - added a small validator for the input of the yearField - changed paintComponent of ImageIcon: 	- adjusting the frame will now maintain ratio 	- border of the imageIcon is now too painted by this method - changed the thickness of the borders of the ImageIcon and the JTextfields to 2

--- MPGI4/src/view/GUI.java
+++ MPGI4/src/view/GUI.java
@@ -1,6 +1,5 @@
 package view;
 
-
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,6 +12,7 @@ import java.io.IOException;
 import layout.TableLayout;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -21,16 +21,18 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
-import javax.swing.border.LineBorder;
+import javax.swing.border.Border;
 
-public class GUI{
-	
-	static File choosenFile =null;
-	
+import exceptions.YearOutOfTimePeriodException;
+
+public class GUI {
+
+	static File choosenFile = null;
+
 	JFrame frame;
-	
+
 	JPanel mainPanel;
-	
+
 	JLabel titleLabel;
 	JLabel albumLabel;
 	JLabel interpretLabel;
@@ -41,115 +43,126 @@ public class GUI{
 	// short guide: new ImageIcon(cover.jpg) -> new JLabel(imageIcon)
 	ImageLabel imageLabel;
 	ImageIcon imageIcon;
-	
+
 	JTextField titleField;
 	JTextField interpretField;
 	JTextField albumField;
 	JTextField yearField;
-	
+
 	JButton save;
-	
+
 	// these strings represent the tags
 	// later they should be moved to MODEL
 	String titleString = "";
 	String interpretString = "";
 	String albumString = "";
 	String yearString = "";
-	
+
 	public GUI() {
-		
+
 		frame = new JFrame("MP3-Tag Editor");
-		
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		frame.setSize(600, 400);
-		
+
 		setGUI();
-		
+
 		frame.setVisible(true);
 
 	}
-	
+
 	public void setGUI() {
-		//Das Layout für das Hauptpanel
+		// Das Layout für das Hauptpanel
 		double[][] layout = {
-				{10, TableLayout.FILL, 5, 5, TableLayout.FILL, 10, TableLayout.FILL, 10},
-				{10, TableLayout.PREFERRED, TableLayout.PREFERRED, 10, TableLayout.PREFERRED, TableLayout.PREFERRED, 10, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.FILL, 10, 10, TableLayout.PREFERRED, 10}	
-		};
+				{ 10, TableLayout.FILL, 5, 5, TableLayout.FILL, 10,
+						TableLayout.FILL, 10 },
+				{ 10, TableLayout.PREFERRED, TableLayout.PREFERRED, 10,
+						TableLayout.PREFERRED, TableLayout.PREFERRED, 10,
+						TableLayout.PREFERRED, TableLayout.PREFERRED,
+						TableLayout.FILL, 10, 10, TableLayout.PREFERRED, 10 } };
 		mainPanel = new JPanel(new TableLayout(layout));
-		
-		//Das Layout für den Button
+
+		// Das Layout für den Button
 		double[][] buttonlayout = {
-				{TableLayout.FILL, 10, TableLayout.PREFERRED},
-				{TableLayout.PREFERRED}
-		};
+				{ TableLayout.FILL, 10, TableLayout.PREFERRED },
+				{ TableLayout.PREFERRED } };
 		JPanel buttonPanel = new JPanel(new TableLayout(buttonlayout));
-		
-		//ein Save button
+
+		// ein Save button
 		save = new JButton("Speichern");
 		save.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("SAVE!!");
-				
+
+				titleString = titleField.getText();
+				interpretString = interpretField.getText();
+				albumString = albumField.getText();
+				yearString = yearField.getText();
+
+				try {
+					int year = Integer.parseInt(yearString);
+					if (year <= 1905 || year > 2012)
+						throw new YearOutOfTimePeriodException();
+				} 
+				// if year is not a number
+				catch (NumberFormatException formatException) {
+					System.out.println("Wrong fromat!");
+				} 
+				// if year is not between 1905 and 2012
+				catch (YearOutOfTimePeriodException yearException) {
+					System.out.println("Year is not valid ");
+				}
+
+				// for debug only
+				System.out.println("" + titleString + ", " + interpretString
+						+ ", " + albumString + ", " + yearString);
 			}
 		});
-		
+
 		titleLabel = new JLabel("Titel");
 		interpretLabel = new JLabel("Interpret");
 		albumLabel = new JLabel("Album");
 		yearLabel = new JLabel("Jahr");
 		coverLabel = new JLabel("Cover");
-		
+
 		// the cover - so far hardcoded
-		// TODO adjust the size of the image 
 		imageLabel = new ImageLabel();
-		File file = new File("./Content/thriller-cover-michael-jackson.jpg");
+		File file = new File("./Content/The-Beatles-�-White-Album.jpg");
 		BufferedImage originalImage = null;
-		try {
-			originalImage = ImageIO.read(file);
-		} catch (IOException e1) {
-		}
+		 try {
+		 originalImage = ImageIO.read(file);
+		 } catch (IOException e1) {
+		 }
 		// if no image is available
-		if (originalImage == null){
+		if (originalImage == null) {
 			imageIcon = new ImageIcon();
 		}
 		// if an image exists
 		else {
 			imageIcon = new ImageIcon(originalImage);
 			imageLabel.setIcon(imageIcon);
-			
-			/*
-			 * Das Bild wird nun in jedem Aufruf von paintComponent neu gezeichnet!
-			 * TODO korrekte Skalierung des Bildes!!
-			 */
-			/////////////////////////////////////////////////////////////////////////////
-			// hier wird�s kribblig... die ersten beiden int-parameter m�ssten sich an //
-			// der Gr��e von TableLayout.FILL orientieren - f�r jede Idee offen!       //
-			/////////////////////////////////////////////////////////////////////////////
-			//imageIcon.setImage(imageIcon.getImage().getScaledInstance(120, 120, Image.SCALE_DEFAULT));
 		}
-		imageLabel.setBorder(new LineBorder(Color.BLACK));
-		
-		
+
 		// Event for choosing a new cover or deleting the existing one
-		imageLabel.addMouseListener(new MouseAdapter(){
-			public void mouseClicked (MouseEvent e){
-				
+		imageLabel.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+
 				switch (e.getButton()) {
 				case MouseEvent.BUTTON1:
-					JFileChooser chooser= new JFileChooser();
+					JFileChooser chooser = new JFileChooser("./Content");
 					int returnVA1 = chooser.showOpenDialog(imageLabel);
-					choosenFile =chooser.getSelectedFile();
+					choosenFile = chooser.getSelectedFile();
 					BufferedImage image = null;
 					try {
 						image = ImageIO.read(choosenFile);
 					} catch (IOException ex) {
 					}
 					imageLabel.setIcon(new ImageIcon(image));
-					break;			
-					//if right mouse button is pressed, delete existing Icon 
+					break;
+				// if right mouse button is pressed, delete existing Icon
 				case MouseEvent.BUTTON3:
 					ImageIcon imageI = new ImageIcon();
 					imageLabel.setIcon(imageI);
@@ -157,71 +170,43 @@ public class GUI{
 					break;
 				}
 			}
-			
+
 		});
-				
-		// the following actionListeners only react on "ENTER"
+
+		Border textFieldBorder = BorderFactory.createLineBorder(Color.BLACK, 2);
 		titleField = new JTextField(20);
-		titleField.addActionListener(new ActionListener() {		
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				titleString = titleField.getText();
-				System.out.println(titleString);
-			}
-		});
-		
+		titleField.setBorder(textFieldBorder);
 		interpretField = new JTextField(20);
-		interpretField.addActionListener(new ActionListener() {		
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				interpretString = interpretField.getText();
-				System.out.println(interpretString);
-			}
-		});
-		
+		interpretField.setBorder(textFieldBorder);
 		albumField = new JTextField(20);
-		albumField.addActionListener(new ActionListener() {		
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				albumString = albumField.getText();
-				System.out.println(albumString);
-			}
-		});
-		
+		albumField.setBorder(textFieldBorder);
 		yearField = new JTextField(20);
-		yearField.addActionListener(new ActionListener() {		
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				yearString = yearField.getText();
-				System.out.println(yearString);
-			}
-		});
-		
-		//Seperatoren-> weglassen oder findet ihr das ok?
+		yearField.setBorder(textFieldBorder);
+
+		// Seperatoren-> weglassen oder findet ihr das ok?
 		mainPanel.add(new JSeparator(JSeparator.VERTICAL), "2,1, 2,12");
 		mainPanel.add(new JSeparator(JSeparator.HORIZONTAL), "4,11, 6,11");
-		
-		//das button panel
+
+		// das button panel
 		buttonPanel.add(save, "2,0");
 		mainPanel.add(buttonPanel, "6,12");
-		
+
 		mainPanel.add(titleLabel, "4,1");
 		mainPanel.add(interpretLabel, "4,4");
 		mainPanel.add(albumLabel, "6,4");
 		mainPanel.add(yearLabel, "6,7");
 		mainPanel.add(coverLabel, "4,7");
-		
+
 		mainPanel.add(imageLabel, "4,8,4,9");
-		
+
 		mainPanel.add(titleField, "4,2,6,1");
 		mainPanel.add(interpretField, "4,5");
 		mainPanel.add(albumField, "6,5, 6,5");
 		mainPanel.add(yearField, "6,8, 6,8");
-		
+
 		frame.add(mainPanel);
 	}
-	
-	
+
 	/**
 	 * @param args
 	 */
--- MPGI4/src/view/ImageLabel.java
+++ MPGI4/src/view/ImageLabel.java
@@ -1,18 +1,22 @@
 package view;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-public class ImageLabel extends JLabel{
-	
-	ImageIcon imageIcon;
+public class ImageLabel extends JLabel {
+
+	private ImageIcon imageIcon;
+
 	public ImageLabel() {
 		super();
 		imageIcon = new ImageIcon();
 	}
-	
+
 	public void setIcon(ImageIcon icon) {
 		super.setIcon(icon);
 		imageIcon = icon;
@@ -21,18 +25,34 @@ public class ImageLabel extends JLabel{
 
 	@Override
 	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		
-		if(imageIcon != null) {
+		Graphics2D g2d = (Graphics2D) g;
+
+		super.paintComponent(g2d);
+
+		// to maintain ratio
+		int width = getWidth();
+		int height = getHeight();
+
+		if (width <= height)
+			height = width;
+		else
+			width = height;
+
+		if (imageIcon != null) {
+			g2d.drawImage(imageIcon.getImage(), 0, 0, width, height, 0, 0,
+					imageIcon.getIconWidth(), imageIcon.getIconHeight(), null);
 			
-			g.drawImage(imageIcon.getImage(), 
-					0, 0, 
-					getWidth(), getHeight(),
-					0, 0,
-					imageIcon.getIconWidth(), imageIcon.getIconHeight(),
-					null);
+			// painting a border
+			g2d.setColor(Color.BLACK);
+			g2d.setStroke(new BasicStroke(2));
+			g2d.drawRect(1, 1, width - 2, height - 2);
+		}
+		
+		else{
+			// painting a border
+			g2d.setColor(Color.BLACK);
+			g2d.setStroke(new BasicStroke(2));
+			g2d.drawRect(2, 2, width - 2, height - 2);
 		}
 	}
-	
-	
 }
